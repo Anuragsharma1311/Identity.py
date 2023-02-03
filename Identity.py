1437f92f210b38dde4a3cb8af4d213955b5f077a
Anuragsharma1311 <124031761+Anuragsharma1311@users.noreply.github.com>
2023-02-03 16:53:11 +0530

    Add files via upload

---
+++ Identity.py
@@ -0,0 +1,28 @@
+x=10
+y=10
+print(x is y)
+print(x is not y)
+
+x=10
+y=15
+print(x is y)
+print(x is not y)
+
+
+x=100
+y=150
+print(x == y)
+print(x != y)
+
+print(x == x)
+print(x != y)
+
+print('Anurag How are you?')
+print('I am fine what about you?')
+print('Fine thank you')
+
+
+x=100
+y=50
+
+print(x-y)
